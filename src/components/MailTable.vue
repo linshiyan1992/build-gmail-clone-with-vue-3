60550342c6103b5d4f4878494714777cc498f4ca
linshiyan1992 <linshiyan1992@gmail.com>
2023-09-18 17:02:05 +0800

    update the package.json and right now the progress is L5-start

---
+++ src/components/MailTable.vue
@@ -0,0 +1,71 @@
+<template>
+  <table class="mail-table">
+    <tbody>
+      <tr
+        v-for="email in unarchivedEmails"
+        :key="email.id"
+        class="clickable"
+        :class="{ read: email.read }"
+        @click="readEmail(email)"
+      >
+        <td>
+          <input type="checkbox" />
+        </td>
+        <td>
+          {{ email.from }}
+        </td>
+        <td>
+          <p>
+            <strong>{{ email.subject }}</strong> - {{ email.body }}
+          </p>
+        </td>
+        <td class="date">
+          {{ format(new Date(email.sentAt), 'MMM do yyyy') }}
+        </td>
+        <td>
+          <button @click="archiveEmail(email)">Archived</button>
+        </td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script>
+import { format } from 'date-fns';
+import axios from 'axios';
+
+export default {
+  name: 'App',
+  async setup() {
+    let { data: emails } = await axios.get('http://localhost:3000/emails');
+
+    return {
+      format,
+      emails,
+    };
+  },
+  computed: {
+    sortedEmails() {
+      return [...this.emails].sort((e1, e2) => {
+        return e1.sentAt < e2.sentAt ? 0 : 1;
+      });
+    },
+    unarchivedEmails() {
+      return this.sortedEmails.filter((email) => email.archived === false);
+    },
+  },
+  methods: {
+    readEmail(email) {
+      email.read = true;
+      this.updateEmail(email);
+    },
+    archiveEmail(email) {
+      email.archived = true;
+      this.updateEmail(email);
+    },
+    updateEmail(email) {
+      axios.put(`http://localhost:3000/emails/${email.id}`, email);
+    },
+  },
+};
+</script>
